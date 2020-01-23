ee0bd24e4a0612cc7df6309e06dac3fbb9129bfa
paulabenites <53128309+paulabenites@users.noreply.github.com>
2020-01-22 20:07:11 -0500

    Generacion factura txt

--- src/modelo/Venta.java
+++ src/modelo/Venta.java
@@ -5,9 +5,15 @@
  */
 package modelo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,14 +29,76 @@ public class Venta {
     private Personal_Caja personalCaja;
     private Forma_pago forma_pago_ID;
     private Cliente cliente;
+    private List<Detalle_Venta> carrito;
     
-    public double calcularMonto(){
-        if(descuento >0){
-            return total*(1 - (descuento/100));
+    public void calcularMontoApagar(List<Detalle_Venta> itemsCarrito){
+        this.carrito = itemsCarrito;
+        Iterator<Detalle_Venta> it = itemsCarrito.iterator();
+        
+        double monto = 0;
+        
+        while(it.hasNext()){
+            Detalle_Venta det = it.next();
+            monto += det.calcularPrecio();
         }
-        return total;
+        
+        this.setSubtotal(monto);
+        this.total = subtotal - descuento;
+    }
+    
+    public boolean comprobarValidesDeVenta(){
+        if(this.cliente == null)
+            return false;
+        if(this.subtotal == 0)
+            return false;
+        return this.personalCaja != null;
+    }
+    
+    
+    
+    public static void generarFactura(Venta v){
+        String ruta = "res/facturas/factura" +v.numeroFactura+".txt";
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        try {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(facturaEncabezado(v));
+            bw.write(productosFactura(v));
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Venta.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+    private static String facturaEncabezado(Venta v){
+        String s = "\t\t*VETERINARIA DR. PELOS*\t\t\n Nro. Factura: "+v.numeroFactura+
+                "\tRUC:0987654321 \n\tGUAYAQUIL-ECUADOR\n"+
+                "Cliente   : "+v.cliente.getNombre()+" "+ v.cliente.getApellido()+
+                "\nCED/RUC  : "+ v.cliente.getCedula()+
+                "\nTelefono : "+ v.cliente.getNum_telefonico()+
+                "\n-----------------------------------------------\n"+
+                String.format("|%-18s|", "Producto")+String.format("|%-8s|", "Cantidad")+String.format("|%-8s|\n", "Pre.Total")+
+                "-----------------------------------------------\n"
+                ;
+        return s;
+    }
+    private static String productosFactura(Venta v){
+        Iterator<Detalle_Venta> it = v.getCarrito().listIterator();
+        StringBuilder sb = new StringBuilder();
+        while(it.hasNext()){
+            Detalle_Venta d = it.next();
+            sb.append(String.format("%-20s %-10s %-10s\n",d.getNombre(),d.getCantidad(),d.calcularPrecio()));
+        }
+        sb.append(String.format("\t Subtotal : %-10s",v.subtotal));
+        sb.append(String.format("\t Descuento: %-10s",v.descuento));
+        sb.append(String.format("\n\t Total    : %-10s",v.total));
+
+        return sb.toString();
     }
 
+    public List<Detalle_Venta> getCarrito() {
+        return carrito;
+    }
+    
     public double getSubtotal() {
         return subtotal;
     }
@@ -86,27 +154,6 @@ public class Venta {
 
     public void setDescuento(double descuento) {
         this.descuento = descuento;
-    }
-    
-    public void calcularMontoApagar(List<Detalle_Venta> itemsCarrito){
-        Iterator<Detalle_Venta> it = itemsCarrito.iterator();
-        
-        double monto = 0;
-        
-        while(it.hasNext()){
-            Detalle_Venta det = it.next();
-            monto += det.calcularPrecio();
-        }
-        
-        this.setSubtotal(monto);
-    }
-    
-    public boolean comprobarValidesDeVenta(){
-        if(this.cliente == null)
-            return false;
-        if(this.subtotal == 0)
-            return false;
-        return this.personalCaja != null;
     }
 
     public Forma_pago getForma_pago_ID() {
